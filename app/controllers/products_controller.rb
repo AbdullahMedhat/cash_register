a32f6d27c7b519a296b7a4654c4e021dd4ca9d8b
Abdullah Medhat <abdullahmedaht@outlook.com>
2022-01-23 16:17:33 +0200

    Add basic UI for the products#index page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,15 @@
 # frozen_string_literal: true
 
 class ProductsController < ApplicationController
+  before_action :set_order, only: :index
+
   def index
     @products = Product.all
+  end
+
+  private
+
+  def set_order
+    @order = Order.first_or_create
   end
 end
